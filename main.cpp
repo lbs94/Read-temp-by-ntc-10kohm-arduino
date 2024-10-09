9d6aaf23fcee05cc915a9538d40c99fe13297f51
lbs94 <1luclevan@gmail.com>
2024-10-09 09:55:00 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,17 +7,25 @@ const float C = 2.019202697e-07;
 const float dientro = 10000;  // Điện trở nối tiếp 10kΩ,có thể chọn cái khác
 
 NTCtemp ntc(dientro,A,B,C);
-
-
-
+// biến để chạy 1 giây 1 lần
+unsigned long previousMillis1 = 0;
+const long interval1 = 1000; //1s
 void setup() {
   // put your setup code here, to run once:
   pinMode(A0,INPUT);
+ Serial.begin(9600); // mở port ở mức 9600
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis1 >= interval1) {
+    previousMillis1 = currentMillis;
   float temperature = ntc.gettemp(A0);  // ví dụ đọc nhiệt độ từ chân A0
+   Serial.print("Nhiet do: ");
+   Serial.println(temperature);
+  }
+
 }
 
 // put function definitions here:
